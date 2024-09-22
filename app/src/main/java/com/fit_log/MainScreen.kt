aa1c9ce81606a3cbd40999838c95a535ea0567a2
raiisoares <raisoares.dev@gmail.com>
2024-09-22 20:03:38 -0300

    feat: add navigation & app structure

---
+++ app/src/main/java/com/fit_log/MainScreen.kt
@@ -0,0 +1,92 @@
+package com.fit_log
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.fit_log.model.NavItem
+import com.fit_log.pages.AboutPage
+import com.fit_log.pages.AddPage
+import com.fit_log.pages.DashboardPage
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+
+    val navItemList = listOf(
+        NavItem("Add", ImageVector.vectorResource(R.drawable.ic_add_black_24dp)),
+        NavItem("Dashboard", ImageVector.vectorResource(R.drawable.ic_dashboard_black_24dp)),
+        NavItem("About", ImageVector.vectorResource(R.drawable.ic_info_black_24dp))
+    )
+
+    var selectedIndex by remember {
+        mutableIntStateOf(1)
+    }
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        val navController = rememberNavController()
+        Column(modifier = Modifier.padding(innerPadding)) {
+            NavHost(
+                navController = navController,
+                startDestination = "Dashboard",
+                modifier = Modifier.weight(1f)
+            ) {
+                composable("Dashboard") {
+                    DashboardPage()
+                }
+
+                composable("Add") {
+                    AddPage()
+                }
+
+                composable("About") {
+                    AboutPage()
+                }
+            }
+
+            BottomAppBar(actions = {
+                NavigationBar {
+                    navItemList.forEachIndexed { index, navItem ->
+                        NavigationBarItem(
+                            selected = selectedIndex == index,
+                            onClick = {
+                                selectedIndex = index
+                                val route = when (index) {
+                                    0 -> "Add"
+                                    1 -> "Dashboard"
+                                    else -> "About"
+                                }
+                                navController.navigate(route, navOptions = navOptions {
+                                    launchSingleTop = true
+                                    popUpTo(navController.graph.id)
+                                })
+                            },
+                            icon = {
+                                Icon(imageVector = navItem.icon, contentDescription = "Icon")
+                            },
+                            label = {
+                                Text(navItem.label)
+                            }
+                        )
+                    }
+                }
+            })
+        }
+    }
+}
