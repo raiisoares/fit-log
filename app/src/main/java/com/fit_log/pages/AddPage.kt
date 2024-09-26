563f23e69ccd9b10b083f3f9734d09a0f5b2dffe
raiisoares <raisoares.dev@gmail.com>
2024-09-26 20:44:55 -0300

    style: refactor on AddPage & add dark mode as default

--- app/src/main/java/com/fit_log/pages/AddPage.kt
+++ app/src/main/java/com/fit_log/pages/AddPage.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -30,9 +31,9 @@ fun AddPage(onAddNote: (String, String, String) -> Unit) {
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
         Text(
             text = "Adicionar notas",
             fontSize = 30.sp,
@@ -43,41 +44,47 @@ fun AddPage(onAddNote: (String, String, String) -> Unit) {
             textAlign = TextAlign.Center
         )
 
-        TextField(
-            value = title,
-            onValueChange = { title = it },
-            label = { Text("Título") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        TextField(
-            value = content,
-            onValueChange = { content = it },
-            label = { Text("Conteúdo") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(min = 100.dp),
-            maxLines = 5,
-            singleLine = false
-        )
-
-        TextField(
-            value = subject,
-            onValueChange = { subject = it },
-            label = { Text("Assunto") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Button(
-            onClick = {
-                onAddNote(title, content, subject)
-                title = ""
-                content = ""
-                subject = ""
-            },
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            modifier = Modifier.padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Adicionar Nota")
+            TextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text("Título") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            TextField(
+                value = content,
+                onValueChange = { content = it },
+                label = { Text("Conteúdo") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(min = 100.dp),
+                maxLines = 5,
+                singleLine = false
+            )
+
+            TextField(
+                value = subject,
+                onValueChange = { subject = it },
+                label = { Text("Assunto") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Button(
+                onClick = {
+                    onAddNote(title, content, subject)
+                    title = ""
+                    content = ""
+                    subject = ""
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Adicionar Nota")
+            }
         }
     }
 }
--- app/src/main/java/com/fit_log/pages/NoteDetailPage.kt
+++ app/src/main/java/com/fit_log/pages/NoteDetailPage.kt
@@ -1,6 +1,9 @@
 package com.fit_log.pages
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
